5a99e8c2b1e3f04d337f34c986d9431f635c3948
DaniilPeatcov2003 <fosh1551342@gmail.com>
2026-05-11 21:08:50 +0300

    Update ConnectionManager.cs

--- MessangerApp/Singleton/ConnectionManager.cs
+++ MessangerApp/Singleton/ConnectionManager.cs
@@ -10,16 +10,13 @@ namespace MessengerApp.Singleton
     {
         private static WebSocketManager uniqueInstance;
 
-        // singletonData
         private List<string> connections;
 
-        // приватный конструктор (запрещает создание объекта извне)
         private WebSocketManager()
         {
             connections = new List<string>();
         }
 
-        // static Instance()
         public static WebSocketManager Instance()
         {
             if (uniqueInstance == null)
@@ -30,14 +27,12 @@ namespace MessengerApp.Singleton
             return uniqueInstance;
         }
 
-        // SingletonOperation()
         public void AddConnection(string user)
         {
             if (!connections.Contains(user))
                 connections.Add(user);
         }
 
-        // GetSingletonData()
         public List<string> GetConnections()
         {
             return connections;
